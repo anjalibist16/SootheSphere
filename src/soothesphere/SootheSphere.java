d71ef8825f437f33d072532576a93b5a9d7bb9f9
Shreeya Paudel <shreeya.paudel.8e@gmail.com>
2024-07-29 11:27:58 +0545

    Updated Sprint 2

--- src/soothesphere/SootheSphere.java
+++ src/soothesphere/SootheSphere.java
@@ -1,46 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package soothesphere;
-<<<<<<< HEAD
-
-/**
- *
- * @author TUF
-=======
-import soothesphere.View.HomePage1;
-import soothesphere.View.LogInPage;
-
-
-
-/**
- *
- * @author User
->>>>>>> 7686dc4a07292e756bad8dad7995b5b384dd7feb
- */
-public class SootheSphere {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-<<<<<<< HEAD
-        listofcontacts lc = new listofcontacts();
-        lc.setVisible(true);
-    }
-    
-=======
-        HomePage1 Hp = new HomePage1();
-        Hp.setVisible(true);
-//        LogInPage Lp = new LogInPage();
-//        Lp.setVisible(true);
-         
-
-
-        
-    }
-   
->>>>>>> 7686dc4a07292e756bad8dad7995b5b384dd7feb
-}
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+// */
+//package soothesphere;
+//<<<<<<< HEAD
+//
+///**
+// *
+// * @author TUF
+//=======
+//import soothesphere.View.HomePage1;
+//import soothesphere.View.LogInPage;
+//
+//
+//
+///**
+// *
+// * @author User
+//>>>>>>> 7686dc4a07292e756bad8dad7995b5b384dd7feb
+// */
+//public class SootheSphere {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        // TODO code application logic here
+//<<<<<<< HEAD
+//        listofcontacts lc = new listofcontacts();
+//        lc.setVisible(true);
+//    }
+//    
+//=======
+//        HomePage1 Hp = new HomePage1();
+//        Hp.setVisible(true);
+////        LogInPage Lp = new LogInPage();
+////        Lp.setVisible(true);
+//         
+//
+//
+//        
+//    }
+//   
+//>>>>>>> 7686dc4a07292e756bad8dad7995b5b384dd7feb
+//}
